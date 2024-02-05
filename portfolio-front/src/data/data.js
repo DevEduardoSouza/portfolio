57d5d274d1eaba0631e19dc1f23c3957b8c16b4d
DevEduardoSouza <eduardosouza.din2@gmail.com>
2024-02-05 11:32:37 -0300

    feat: add dados

---
+++ portfolio-front/src/data/data.js
@@ -0,0 +1,40 @@
+export const data = {
+  projects: [
+    {
+      title: "Landing Page do Apple",
+      description:
+        "Utilizando HTML, CSS e JavaScript, o projeto tem como objetivo simular a apresentação e detalhamento do produto de forma atraente e responsiva.",
+      link: {
+        gitHub: "",
+        deploy: "https://landing-page-apple-watch.netlify.app/",
+      },
+      img: "https://github.com/DevEduardoSouza/apple-watch-landing-page/blob/main/img/img_projeto.png?raw=true",
+      priority: 1,
+      isNew: false,
+    },
+    {
+      title: "Venda de Planos",
+      description:
+        " O template foi criado para uma página de venda de planos ou serviços, apresentando informações sobre os planos disponíveis e seus benefícios.",
+      link: {
+        gitHub: "",
+        deploy: "https://deveduardosouza.github.io/rachi/",
+      },
+      img: "https://github.com/DevEduardoSouza/rachi/raw/main/image/imgs-projeto/screencapture-rachi-projeto-netlify-app-2023-12-desktop.png",
+      priority: 3,
+      isNew: false,
+    },
+    {
+      title: "GeekInsider - Plataforma de Notícias Geek",
+      description:
+        " Bem-vindo ao GeekInsider, um projeto de blog de notícias voltado para a cultura geek.",
+      link: {
+        gitHub: "",
+        deploy: "https://geek-insider.netlify.app/",
+      },
+      img: "https://github.com/DevEduardoSouza/GeekInsider/raw/main/imgs/img-projeto.jpeg",
+      priority: 2,
+      isNew: false,
+    },
+  ],
+};
