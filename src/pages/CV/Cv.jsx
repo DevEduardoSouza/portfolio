709bf94b4082fe002d65396c7435e70709aae247
DevEduardoSouza <eduardosouza.din2@gmail.com>
2024-06-08 13:18:36 -0300

    feat: add novas experiências3

--- src/pages/CV/Cv.jsx
+++ src/pages/CV/Cv.jsx
@@ -16,7 +16,7 @@ export const Cv = () => {
           <Section>
             <Title>Eduardo Souza</Title>
             <Paragraph>
-              Desenvolvedor ReactJS | TypeScript | HTML | CSS | javaScript |
+              Desenvolvedor ReactJS | TypeScript | HTML | CSS | JavaScript |
               NodeJs
             </Paragraph>
           </Section>
@@ -39,22 +39,40 @@ export const Cv = () => {
           </Section>
 
           <Section margin="2rem 0">
-            <Title>Experiência</Title>
+            <Title>Experiências</Title>
+      
             <Paragraph>
-              Vulcan <br /> de 2024 - Present
+              Empório das Rendas <br /> 2023  / 2024
             </Paragraph>
             <Paragraph>
-              Como parte da equipe de desenvolvimento do site da comunidade
-              Vulcan, tenho contribuído para criar uma plataforma onde os
-              autores possam publicar seus capítulos de webnovel e os usuários
-              possam desfrutar de uma interface intuitiva e envolvente. Estamos
-              utilizando ReactJS para construir uma experiência moderna e
-              responsiva para todos os membros da comunidade.
+              Desenvolvi plugins e realizei modificações no site em WordPress,
+              criando um catalogador de produtos para o cliente. Também implementei
+              um calculador de impostos, lucros, despesas e custos na aba de
+              administração do WordPress, permitindo que o cliente tivesse dados
+              personalizados sobre seus produtos.
             </Paragraph>
             <Paragraph>
               <Link
                 style={{ color: "#f3b410" }}
-                to={"https://github.com/VulcanAsu/illlusia-front-end"}
+                to={"https://emporiodasrendas.com.br/"}
+                target="_blank"
+              >
+                link para o site
+              </Link>
+            </Paragraph>
+
+            <Paragraph>
+              MapQuest <br /> 2024 - Em desenvolvimento
+            </Paragraph>
+            <Paragraph>
+              Atualmente estou criando um jogo com inspirações no GeoGuessr no
+              navegador usando TypeScript. Meu objetivo é lançar o jogo ainda este
+              ano e monetizá-lo.
+            </Paragraph>
+            <Paragraph>
+              <Link
+                style={{ color: "#f3b410" }}
+                to={"https://github.com/DevEduardoSouza/MapQuest"}
                 target="_blank"
               >
                 link para github
@@ -65,9 +83,8 @@ export const Cv = () => {
           <Section margin="2rem 0">
             <Title>Formação acadêmica</Title>
             <Paragraph>
-              Formação acadêmica IFBA - Instituto Federal da Bahia Tecnólogo,
-              Analise e desenvolvimento de sistemas · (março de 2022 - julho de
-              2025)
+              IFBA - Instituto Federal da Bahia <br />
+              Tecnólogo, Análise e Desenvolvimento de Sistemas (março de 2022 - julho de 2025)
             </Paragraph>
           </Section>
         </Wrapper>
